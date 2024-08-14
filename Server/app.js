8f6f8274f25ed09da6ff2ad122b3c7002048b7ba
Aadesh Gulumbe <71577243+aadesh0706@users.noreply.github.com>
2024-08-14 14:12:59 +0530

    Update app.js

--- Server/app.js
+++ Server/app.js
@@ -9,19 +9,16 @@ const app = express();
 //   origin: 'https://booksnap-mu.vercel.app/', // Your frontend URL
 // }));
 // const cors = require('cors');
-// app.use(cors());
-app.use(cors({
-  origin: 'https://booksnap-mu.vercel.app', // Your frontend URL without trailing slash
-}));
-const corsOptions = {
-  origin: 'https://booksnap-mu.vercel.app',
+// app.use(cors());const corsOptions = {
+  origin: 'https://booksnap-3egyf0a4i-aadesh0706s-projects.vercel.app',
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
   allowedHeaders: 'Content-Type,Authorization',
-  optionsSuccessStatus: 204 // Some legacy browsers (e.g., IE11) choke on 204
+  optionsSuccessStatus: 200 // Set to 200 for successful OPTIONS requests
 };
 
 app.use(cors(corsOptions));
 app.options('*', cors(corsOptions)); // enable pre-flight across-the-board
+
 
 // Routes
 const authRoutes = require("./routes/auth");
