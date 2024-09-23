e045e63ef940f7863851fcc3062434e975f3265b
Aadesh Gulumbe <71577243+aadesh0706@users.noreply.github.com>
2024-09-23 15:16:46 +0530

    Update main.jsx aadesh

--- Fronted/AppReact/src/main.jsx
+++ Fronted/AppReact/src/main.jsx
@@ -1,78 +1,59 @@
-import './index.css'
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import {createBrowserRouter, createRoutesFromElements, Route,RouterProvider,Routes} from 'react-router-dom'
-import {Provider} from 'react-redux'
-import store from './store/store.js'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
+import './index.css';
+import store from './store/store.js';
 
-import Layout from './Layout.jsx'
-import Notes from './pages/Notes/Notes.jsx'
-import Pdf1 from './pages/Pdf/Pdf1.jsx'
+import Layout from './Layout.jsx';
+import Notes from './pages/Notes/Notes.jsx';
+import Pdf1 from './pages/Pdf/Pdf1.jsx';
 
+import PdfContextProvider from './context/pdfContext/PdfContextProvider.jsx';
+import AboutUs from './pages/AboutUs/AboutUs.jsx';
+import ContactUs from './pages/ContactUs/ContactUs.jsx';
+import Home from './pages/Home/Home.jsx';
+import Login from './pages/Login/Login.jsx';
+import NotesSems from './pages/NotesSem/NotesSems.jsx';
+import NotesSubjects from './pages/NotesSubject/NotesSubjects.jsx';
+import SignUp from './pages/SignUp/SignUp.jsx';
+import UnderDevelopment from './pages/UnderDevelopment/UnderDevelopment.jsx';
 
-import PdfContextProvider from './context/pdfContext/PdfContextProvider.jsx'
-import NotesSems from './pages/NotesSem/NotesSems.jsx'
-import NotesSubjects from './pages/NotesSubject/NotesSubjects.jsx'
-import Login from './pages/Login/Login.jsx'
-import SignUp from './pages/SignUp/SignUp.jsx'
-import Home from './pages/Home/Home.jsx'
-import AboutUs from './pages/AboutUs/AboutUs.jsx'
-import ContactUs from './pages/ContactUs/ContactUs.jsx'
-import UnderDevelopment from './pages/UnderDevelopment/UnderDevelopment.jsx'
+import { ClerkProvider } from '@clerk/clerk-react'; // Import ClerkProvider
 
-//import ScrollToTop from './components/ScrollToTop.js'
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
+if (!PUBLISHABLE_KEY) {
+  throw new Error('Missing Clerk Publishable Key');
+}
+
+// Define the router
 const router = createBrowserRouter(
   createRoutesFromElements(
-
-      // <Route path='/' element={<Layout />}>
-      //   <Route path='notes' element={<Notes />}>
-      //     <Route path=':branch' element={<NotesSems />}>
-      //       <Route path=':semester' element={<NotesSubjects />} />
-      //     </Route>
-      //   </Route>
-      //   <Route path='pdf' element={<Pdf1 />} />
-      // </Route>
-
-
-
-    
-    <Route path='/' element={<Layout/>}>
-      <Route path='/' element={<Home/>}/>
-      <Route path='notes' element={<Notes/>}/>
-        {/* <Route path='sem/:semester'  */}
-        {/* <Route path='notes/sem' element={<NotesSems/>} > */}
-      <Route path='notes/:branch' element={<NotesSems/>} />
-        {/* <Route path='notes/sem/:semester' element={<NotesSubjects/>} /> */}
-      <Route path='notes/:branch/:semester' element={<NotesSubjects/>} />
-      <Route path='pdf' element={<Pdf1/>}/>
-      <Route path='login' element={<Login/>}/>
-      <Route path='signup' element={<SignUp/>}/>
-      <Route path='aboutus' element={<AboutUs/>}/>
-      <Route path='contactus' element={<ContactUs/>}/>
-      <Route path='community' element={<UnderDevelopment/>}/>
-      <Route path='shop' element={<UnderDevelopment/>}/>
+    <Route path='/' element={<Layout />}>
+      <Route path='/' element={<Home />} />
+      <Route path='notes' element={<Notes />} />
+      <Route path='notes/:branch' element={<NotesSems />} />
+      <Route path='notes/:branch/:semester' element={<NotesSubjects />} />
+      <Route path='pdf' element={<Pdf1 />} />
+      <Route path='login' element={<Login />} />
+      <Route path='signup' element={<SignUp />} />
+      <Route path='aboutus' element={<AboutUs />} />
+      <Route path='contactus' element={<ContactUs />} />
+      <Route path='community' element={<UnderDevelopment />} />
+      <Route path='shop' element={<UnderDevelopment />} />
     </Route>
-
-
-
-
-    // <Route path='/' element={<Layout/>}>
-    //   <Route path='notes' element={<Notes/>}/>
-    //   <Route path='pdf' element={<Pdf1/>}/>
-    // </Route>
   )
-
-)
+);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <PdfContextProvider>
-        <RouterProvider router = {router}/>
-      </PdfContextProvider>
-    </Provider>
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl='/'>
+      <Provider store={store}>
+        <PdfContextProvider>
+          <RouterProvider router={router} />
+        </PdfContextProvider>
+      </Provider>
+    </ClerkProvider>
   </React.StrictMode>
-)
-
-
+);
